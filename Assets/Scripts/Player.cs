5d7f10e83ccd9fc48e3df27e07203bbe64a57ffd
unknown <tanner-olsen@hotmail.com>
2021-05-25 19:56:30 -0400

    Added basic left and right movement - Edited Player Script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public float moveSpeed = 0.0f;
+    private float maxSpeed = 10.0f;
+    public float jump;
+    private Rigidbody2D rigid;
+    private Player player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
+        player = GetComponent<Player>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+            //rigid.AddForce(Vector2.right);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //transform.Translate(Vector2.up * jump * Time.deltaTime );
+            rigid.AddForce(Vector2.up * jump);
+        }
     }
 }

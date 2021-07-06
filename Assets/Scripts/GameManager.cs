668d65c37fac3dafe7b1616a454042f056e2f80d
snickk95 <51108751+snickk95@users.noreply.github.com>
2021-07-06 12:20:20 -0400

    added pause function no ability to unpause yet

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,6 +13,25 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+    
+    //function to set time scale to 0 so nothing moves well in pause menu
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+        AudioListener.pause = true; //stops the audio of the game as well
+        Debug.Log("game paused");
+    }
+
+    // function to restart the timescale so we can resume play
+    void Resume()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false; //starts the game audio again
+        Debug.Log("game not paused");
     }
 }

42df16fcdd2d4da1bbd24cd471159ac865bd9109
snickk95 <51108751+snickk95@users.noreply.github.com>
2021-07-06 13:37:12 -0400

    continue button working in pause menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+  
     // Start is called before the first frame update
     void Start()
     {
@@ -13,25 +15,8 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            PauseGame();
-        }
+       
     }
     
-    //function to set time scale to 0 so nothing moves well in pause menu
-    void PauseGame()
-    {
-        Time.timeScale = 0;
-        AudioListener.pause = true; //stops the audio of the game as well
-        Debug.Log("game paused");
-    }
-
-    // function to restart the timescale so we can resume play
-    void Resume()
-    {
-        Time.timeScale = 1;
-        AudioListener.pause = false; //starts the game audio again
-        Debug.Log("game not paused");
-    }
+   
 }
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject Pause; // use serialize field to make privbate but alsao visable in editor
+  
+    //function to set time scale to 0 so nothing moves well in pause menu
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        AudioListener.pause = true; //stops the audio of the game as well
+        Debug.Log("game paused");
+        Pause.SetActive(true); // activate pause menu
+    }
+
+    // function to restart the timescale so we can resume play
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false; //starts the game audio again
+        Debug.Log("game not paused");
+        Pause.SetActive(false); //disable pause menu
+    }
+
+    //function to quit the game
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}

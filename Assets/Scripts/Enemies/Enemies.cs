659ce5fc5063549d4823c284ac4c40f9fdae9305
maddogmitch <mightymitch66@gmail.com>
2021-08-18 12:28:03 -0400

    Added base enemy movent and ints for health and damage

---
+++ Assets/Scripts/Enemies/Enemies.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+    public int health;
+    public int damage;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Vector3[] positions;
+
+    private int index;
+
+    void EnemeyMovement()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, positions[index], Time.deltaTime * speed);
+
+        if (transform.position == positions[index])
+        {
+            if(index == positions.Length -1)
+            {
+                index = 0;
+            }
+            else
+            {
+                index++;
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        EnemeyMovement();
+    }
+}
